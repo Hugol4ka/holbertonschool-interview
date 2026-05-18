70ff9ea9533f45e3ef76186583d89ca6ebeb17ea
Hugol4ka <hugolaca12300@gmail.com>
2026-05-18 13:19:56 +0200

    Add implementation of canUnlockAll function in lockboxes.py

---
+++ lockboxes.py/0-lockboxes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def canUnlockAll(boxes):
+    boites_ouvertes = set([0])
+    cles_a_verifier = [0]
+
+    while cles_a_verifier:
+        boite_actuelle = cles_a_verifier.pop()
+        for cle in boxes[boite_actuelle]:
+            if cle not in boites_ouvertes:
+                boites_ouvertes.add(cle)
+                cles_a_verifier.append(cle)
+    return len(boites_ouvertes) == len(boxes)
