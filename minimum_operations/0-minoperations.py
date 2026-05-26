0fd68115225a6a13df002ec5555500ae28b28acc
Hugol4ka <hugolaca12300@gmail.com>
2026-05-26 16:01:54 +0200

    Add minOperations function to calculate minimum operations for n characteristics

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+def minOperations(n):
+    """
+    Calculate the minimum number of operations to obtain
+    exactly n characteristics H
+    """
+    if n <= 1:
+        return 0
+
+    num_ops = 0
+    div = 2
+    while n > 1:
+        while n % div == 0:
+            num_ops = num_ops + div
+            n //= div
+        div = div + 1
+    return num_ops
