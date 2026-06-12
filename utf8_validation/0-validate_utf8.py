fa106234bc1db4ac7c0ab3fcffb8db9ed3ca6190
Hugol4ka <hugolaca12300@gmail.com>
2026-06-12 11:27:30 +0200

    Add 0-validate_utf8.py for Checks if a given data set represents a valid UTF-8 encoding.

---
+++ utf8_validation/0-validate_utf8.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+def validUTF8(data):
+    """Checks if a given data set represents a valid UTF-8 encoding."""
+    num_octets = 0
+
+    for octet in data:
+
+        if num_octets > 0:
+            if (octet >> 6) != 2:
+                return False
+            num_octets = num_octets - 1
+        else:
+            if (octet >> 5) == 6:
+                num_octets = 1
+            elif (octet >> 4) == 14:
+                num_octets = 2
+            elif (octet >> 3) == 30:
+                num_octets = 3
+            elif (octet >> 7) == 0:
+                num_octets = 0
+            else:
+                return False
+        
+    return num_octets == 0
