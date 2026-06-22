8894113f0d63597a3ac1418666d9f7dc91711364
Hugol4ka <hugolaca12300@gmail.com>
2026-06-22 15:07:02 +0200

    Fix semistandard validation

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -7,15 +7,14 @@ request(url, function (error, response, body) {
     const filmData = JSON.parse(body);
     const characters = filmData.characters;
 
-
-    function fetchCharacter(index) {
+    function fetchCharacter (index) {
       if (index >= characters.length) return;
 
       request(characters[index], function (charError, charResponse, charBody) {
         if (!charError) {
           const charData = JSON.parse(charBody);
           console.log(charData.name);
-          
+
           fetchCharacter(index + 1);
         }
       });
