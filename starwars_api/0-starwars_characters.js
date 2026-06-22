dcec0ae48da1ecb6b4c0f683e41e8c610af6c23e
Hugol4ka <hugolaca12300@gmail.com>
2026-06-22 14:59:40 +0200

    Add files 0-starwars_characters.js

---
+++ starwars_api/0-starwars_characters.js
@@ -0,0 +1,24 @@
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/';
+
+request(url, function (error, response, body) {
+  if (!error) {
+    const filmData = JSON.parse(body);
+    const characters = filmData.characters;
+
+
+    function fetchCharacter(index) {
+      if (index >= characters.length) return;
+
+      request(characters[index], function (charError, charResponse, charBody) {
+        if (!charError) {
+          const charData = JSON.parse(charBody);
+          console.log(charData.name);
+          
+          fetchCharacter(index + 1);
+        }
+      });
+    }
+    fetchCharacter(0);
+  }
+});
